1243cf6b859c7cb4fc4cce44fdb0d81981983eed
Adonis K <aklp08@gmail.com>
2015-02-09 23:38:49 +0200

    parse all files inside .posts, remove all non '.md' ones and save them markdown ones in an array to be parsed later

--- generate.js
+++ generate.js
@@ -13,6 +13,26 @@ marked.setOptions({
 
 var layout = fs.readFileSync('./templates/layout.jade', 'utf8');
 
+var postsPath = './posts/';
+var postDir = fs.readdirSync(postsPath);
+
+var checkFileExtension = function (files, extension) {
+    'use strict';
+
+    var result = files.filter(function (file) {
+        return file.substr(-extension.length) === extension;
+    });
+
+    return result;
+};
+
+var posts = checkFileExtension(postDir, '.md');
+
+posts = posts.map(function (post) {
+    'use strict';
+    return fs.readFileSync('./posts/' + post, 'utf8');
+});
+
 var result = jade.render(layout, {
     keywords: 'a, b, c',
     author: 'Adonis K.',
