6c409f0bf393f62566648a4677f4705de77b07fe
Adonis K <aklp08@gmail.com>
2015-02-10 21:44:17 +0200

    major refactor to the code using promises, currently builds html files for all posts

--- generate.js
+++ generate.js
@@ -3,6 +3,8 @@
 var marked = require('marked');
 var jade = require('jade');
 var fs = require('fs');
+var mkdirp = require('mkdirp');
+var Promise = require('bluebird');
 
 // var highlight = require('highlight.js');
 
@@ -11,32 +13,99 @@ marked.setOptions({
 	// highlight: function (code) { return highlight.highlightAuto(code).value; }
 });
 
-var layout = fs.readFileSync('./templates/layout.jade', 'utf8');
-
-var postsPath = './posts/';
-var postDir = fs.readdirSync(postsPath);
-
-var checkFileExtension = function (files, extension) {
-    'use strict';
-
-    var result = files.filter(function (file) {
-        return file.substr(-extension.length) === extension;
-    });
-
-    return result;
+var config = {
+    'path': './posts/',
+    'dist': './build/',
+    'extension': '.md'
 };
 
-var posts = checkFileExtension(postDir, '.md');
+var layout = fs.readFileSync('./templates/layout.jade', 'utf8');
 
-posts = posts.map(function (post) {
+var generateBlog = function () {
     'use strict';
-    return fs.readFileSync('./posts/' + post, 'utf8');
-});
 
-var result = jade.render(layout, {
-    keywords: 'a, b, c',
-    author: 'Adonis K.',
-    description: ''
-});
+    return new Promise(function (resolve, reject) {
+        var directory = fs.readdirSync(config.path);
 
-console.log(result);
+        if (directory) {
+            resolve(directory);
+        } else {
+            reject();
+        }
+    });
+};
+
+var getPostNames = function (directory) {
+    'use strict';
+
+
+    return new Promise(function (resolve, reject) {
+        var posts = directory.filter(function (file) {
+            return file.substr(-config.extension.length) === config.extension;
+        });
+
+        if (!posts.length) {
+            reject();
+        }
+
+        resolve(posts);
+    });
+};
+
+var getPosts = function (posts) {
+    'use strict';
+
+    return new Promise(function (resolve, reject) {
+        posts = posts.map(function (post) {
+            var postName = post.replace(config.extension, '');
+            var postData = fs.readFileSync(config.path + postName + config.extension, 'utf8');
+
+            return {
+                name: postName,
+                data: postData,
+                extension: config.extension
+            };
+        });
+
+        if (!posts.length) {
+            reject();
+        }
+
+        resolve(posts);
+    });
+};
+
+var buildPosts = function (posts) {
+    'use strict';
+
+    return new Promise(function (resolve, reject) {
+        mkdirp(config.dist, function (err) {
+            if (err) {
+                reject();
+            } else {
+                posts.forEach(function (post) {
+                    try {
+                        fs.writeFileSync(config.dist + post.name + '.html', marked(post.data), 'utf8');
+                    } catch(err) {
+                        reject(err);
+                    }
+
+                    resolve();
+                });
+            }
+        });
+    });
+};
+
+generateBlog(config.path)
+    .then(getPostNames)
+    .then(getPosts)
+    .then(buildPosts);
+
+// var result = jade.render(layout, {
+//     keywords: 'a, b, c',
+//     author: 'Adonis K.',
+//     description: ''
+// });
+
+// console.log(result);
