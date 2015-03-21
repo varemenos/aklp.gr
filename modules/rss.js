446257c98499002e302aaeb1d9833dd448e1c4da
Adonis K <aklp08@gmail.com>
2015-03-21 22:22:05 +0200

    avoid overlapping of variable names

--- modules/rss.js
+++ modules/rss.js
@@ -36,9 +36,9 @@ var populateFeed = function (config) {
 var writeFeed = function (config) {
     'use strict';
 
-    var path = path.resolve(config.pwd + config.dist + '/rss.xml');
+    var writePath = path.resolve(config.pwd + config.dist + '/rss.xml');
     var xml = config.feed.xml({indent: true});
-    fs.writeFileSync(path, xml);
+    fs.writeFileSync(writePath, xml);
 };
 
 var rss = function (config) {
